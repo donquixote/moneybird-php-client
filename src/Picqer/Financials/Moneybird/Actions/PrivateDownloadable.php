f32057964fcfce99fdb7b7dbe600b86e235c4858
Andreas Hennings <andreas@dqxtech.net>
2018-01-06 01:56:23 +0100

    #126: Use BaseTrait->connection() instead of ->connection property in traits.

--- src/Picqer/Financials/Moneybird/Actions/PrivateDownloadable.php
+++ src/Picqer/Financials/Moneybird/Actions/PrivateDownloadable.php
@@ -26,7 +26,7 @@ trait PrivateDownloadable
      */
     public function download()
     {
-        $connection = $this->connection;
+        $connection = $this->connection();
         $client = $connection->connect();
         
         $headers = [
